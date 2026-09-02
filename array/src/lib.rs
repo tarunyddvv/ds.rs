8b82dd39f265db28ad097a7a331b48282d3930ff
tunedwtf <293797631+tunedwtf@users.noreply.github.com>
2026-09-02 07:48:04 +0530

    array: push when cap is 0

--- array/src/lib.rs
+++ array/src/lib.rs
@@ -1,3 +1,4 @@
+use std::alloc;
 use std::ptr::NonNull;
 
 pub struct Array<T> {
@@ -12,6 +13,28 @@ impl<T> Array<T> {
             ptr: NonNull::dangling(),
             len: 0,
             capacity: 0,
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        assert_ne!(std::mem::size_of::<T>(), 0, "No zero sized types");
+
+        if self.capacity == 0 {
+            let layout = alloc::Layout::array::<T>(4)
+                .expect("overflow happened could not allocate the array");
+
+            // SAFETY: the layout is hardcoded to be 4 * size_of<T> and
+            // SAFETY: size_of<T> is > 0
+            let ptr = unsafe { alloc::alloc(layout) } as *mut T;
+            let ptr =
+                NonNull::new(ptr).expect("alloc returned null ptr, could not allocate memory");
+
+            // SAFETY: ptr is non-null and we have just allocated enough space for this item.
+            unsafe { ptr.as_ptr().write(elem) };
+
+            self.ptr = ptr;
+            self.len = 1;
+            self.capacity = 4;
         }
     }
 

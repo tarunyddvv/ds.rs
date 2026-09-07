c1c4d8c5a97bb1efedf71ad18349744e17e3d003
tarunyddvv <293797631+tarunyddvv@users.noreply.github.com>
2026-09-07 21:22:59 +0530

    stack-c: pop

--- stack/main.c
+++ stack/main.c
@@ -1,70 +1,90 @@
+#include <_stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct Stack {
-    int size;
-    int top;
-    int* arr;
+  int size;
+  int top;
+  int *arr;
 };
 
-
 int isFull(struct Stack *st) {
-    if (st->top == st->size-1) return 1;
-    return 0;
+  if (st->top == st->size - 1)
+    return 1;
+  return 0;
 }
 
 int isEmpty(struct Stack *st) {
-    if (st->top == -1) return 1;
-    return 0;
+  if (st->top == -1)
+    return 1;
+  return 0;
 }
 
 void push(struct Stack *st, int elem) {
-    if(isFull(st)) {
-        printf("stack is full\n");
-        return;
+  if (isFull(st)) {
+    printf("stack is full\n");
+    return;
+  }
+  st->top++;
+  st->arr[st->top] = elem;
+}
+
+int pop(struct Stack *st) {
+    if(st->top == -1) {
+        printf("stack is empty");
+        free(st);
     }
-    st->top++;
-    st->arr[st->top] = elem;
+
+    int elem = st->arr[st->top];
+    st->arr[st->top] = 0;
+    st->top--;
+
+    return elem;
 }
 
 int top(struct Stack *st) {
-    if(isEmpty(st)) {
-        printf("stack is empty");
-        return -1;
-    }
+  if (isEmpty(st)) {
+    printf("stack is empty");
+    return -1;
+  }
 
-    return st->arr[st->top];
+  return st->arr[st->top];
 }
 
 void display(struct Stack *st) {
-    for(int i=0;i<st->size;i++){
-        printf("| %d | ", st->arr[i]);
-    }
-    printf("\n");
+  for (int i = 0; i <= st->top; i++) {
+    printf("| %d | ", st->arr[i]);
+  }
+  printf("\n");
 }
 
-int main()
-{
-    struct Stack *st = (struct Stack *)malloc(5 * sizeof(int));
+int main() {
+  struct Stack *st = (struct Stack *)malloc(5 * sizeof(int));
 
-    int *arr = (int *)malloc(5 * sizeof(int));
-    st->size = 5;
-    st->top = -1;
-    st->arr = arr;
+  int *arr = (int *)malloc(5 * sizeof(int));
+  st->size = 5;
+  st->top = -1;
+  st->arr = arr;
 
-    push(st, 1);
-    push(st, 2);
-    push(st, 3);
-    push(st, 4);
-    push(st, 5);
-    push(st, 6);
+  push(st, 1);
+  push(st, 2);
+  push(st, 3);
+  push(st, 4);
+  push(st, 5);
+  push(st, 6);
 
-    display(st);
+  display(st);
 
-    printf("%d\n", top(st));
+  printf("%d\n", top(st));
 
-    free(arr);
-    free(st);
+  printf("removed elem: %d\n", pop(st));
 
-    return 0;
+  printf("removed elem: %d\n", pop(st));
+
+  display(st);
+
+  free(arr);
+  free(st);
+
+  return 0;
 }

a37ca59c225aca28cbf4381e5b684682e6b7aa4f
tarunyddvv <293797631+tarunyddvv@users.noreply.github.com>
2026-09-12 10:34:17 +0530

    trees-c: better search

--- trees/bst.c
+++ trees/bst.c
@@ -6,78 +6,20 @@ struct Tree {
   struct TNode *root;
 };
 
-void insert(struct Tree *tree, int elem) {
-  struct Queue *q = (struct Queue *)malloc(sizeof(struct Queue));
-  q->head = NULL;
-  q->tail = NULL;
-  q->len = 0;
-
-  struct TNode *temp = NULL, *ptr = NULL;
-  int x = 0;
-
-  struct TNode *root = (struct TNode *)malloc(sizeof(struct TNode));
-  root->left = NULL;
-  root->data = elem;
-  root->right = NULL;
-
-  tree->root = root;
-  enqueue(q, root);
-
-  while (!isEmpty(q)) {
-    ptr = dequeue(q);
-
-    printf("enter left node for %d: ", ptr->data);
-    fflush(stdout);
-    if (scanf("%d", &x) != 1) {
-      ptr->left = NULL;
-      ptr->right = NULL;
-      break;
-    }
-    if (x != -1) {
-      temp = (struct TNode *)malloc(sizeof(struct TNode));
-      temp->data = x;
-      temp->left = NULL;
-      temp->right = NULL;
-      ptr->left = temp;
-      enqueue(q, temp);
-    } else {
-      ptr->left = NULL;
-    }
-
-    printf("enter right node for %d: ", ptr->data);
-    fflush(stdout);
-    if (scanf("%d", &x) != 1) {
-      ptr->right = NULL;
-      break;
-    }
-    if (x == -1) {
-      ptr->right = NULL;
-      continue;
-    }
-    temp = (struct TNode *)malloc(sizeof(struct TNode));
-    temp->data = x;
-    temp->left = NULL;
-    temp->right = NULL;
-    ptr->right = temp;
-    enqueue(q, temp);
-  }
-  free(q);
-}
-
-int search(struct TNode *root, int elem) {
+struct TNode* search(struct TNode *root, int elem) {
     struct TNode *temp = root;
 
     while(temp != NULL) {
-        if (temp->data == elem) return 1;
+        if (temp->data == elem) return temp;
         if (temp->data > elem)
             temp = temp->left;
         else
             temp = temp->right;
     }
-    return 0;
+    return NULL;
 }
 
-void insertAt(struct TNode **root, int elem) {
+void insert(struct TNode **root, int elem) {
     struct TNode *node = (struct TNode *)malloc(sizeof(struct TNode));
     node->left = NULL;
     node->data = elem;
@@ -147,24 +89,32 @@ int main()
 {
     struct Tree *tree = (struct Tree *)malloc(sizeof(struct Tree));
     tree->root = NULL;
-    insert(tree, 30);
 
-    // if (search(tree->root, 12)) {
-    //     printf("12 is present in the tree\n");
-    // } else {
-    //     printf("12 is not present in the tree\n");
-    // }
-
-    // insertAt(&tree->root, 30);
-    // insertAt(&tree->root, 20);
-    // insertAt(&tree->root, 40);
-    // insertAt(&tree->root, 10);
-    // insertAt(&tree->root, 25);
-    // insertAt(&tree->root, 35);
-    // insertAt(&tree->root, 50);
-    insertAt(&tree->root, 38);
+    insert(&tree->root, 30);
+    insert(&tree->root, 20);
+    insert(&tree->root, 40);
+    insert(&tree->root, 10);
+    insert(&tree->root, 25);
+    insert(&tree->root, 35);
+    insert(&tree->root, 50);
+    insert(&tree->root, 38);
 
     levelOrder(tree->root);
+
+    struct TNode *result = search(tree->root, 40);
+    if (result == NULL) {
+        printf("40 not found\n");
+    } else {
+        printf("elem: %d\n", result->data);
+        if (result->left)
+            printf("elem left data: %d\n", result->left->data);
+        else
+            printf("elem has no left child\n");
+        if (result->right)
+            printf("elem right data: %d\n", result->right->data);
+        else
+            printf("elem has no right child\n");
+    }
 
     freeTree(tree->root);
     free(tree);
